ad38dcc8d5183aa6ae23f23bdc5a191618b5cd59
Vinícius-Freire <120794204+vinicius-freire-oliveira@users.noreply.github.com>
2024-07-09 08:19:41 -0300

    Add files via upload
    O código importa a biblioteca 'time', que permite medir o tempo de execução

---
+++ tempo_execucao.py
@@ -0,0 +1,14 @@
+# Importando a biblioteca 'time', que permite medir o tempo de execução
+import time
+
+# Registrando o tempo de início da execução
+start = time.time()
+
+# Imprimindo uma mensagem qualquer
+print("O tempo usado para executar isto é dado abaixo")
+
+# Registrando o tempo de término da execução
+end = time.time()
+
+# Calculando e imprimindo o tempo de execução em segundos
+print(end - start)
